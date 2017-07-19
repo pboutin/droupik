ea24df4b666a45d74466681d2f5674d4bde952d1
Pascal Boutin <contact@pboutin.net>
2017-07-18 22:07:02 -0400

    Introduce new almanax commands

--- app/controllers/almanax.js
+++ app/controllers/almanax.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const get = require('lodash').get;
+const map = require('lodash').map;
+const deburr = require('lodash').deburr;
 const lowerFirst = require('lodash').lowerFirst;
 
 const almanaxData = require('./../../node_modules/dofus-data/almanax-data.json');
@@ -29,6 +31,52 @@ class Almanax {
             return `Demain, ${this._trimBonus(almanax.bonus)}.\nL'offrande sera : ${almanax.quest}.`;
         }
         throw `Almanax not found for ${tomorrow}`;
+    }
+
+    getNextDropEvents() {
+        return this._formattedFiveFirstMatching(almanaxBonus => {
+            return /(butin|challenge|.toile)/.test(almanaxBonus);
+        });
+    }
+
+    getNextXpEvents() {
+        return this._formattedFiveFirstMatching(almanaxBonus => {
+            return /(exp.rience|challenge|.toile)/.test(almanaxBonus);
+        });
+    }
+
+    getNextEventsForKeyword(keyword) {
+        return this._formattedFiveFirstMatching(almanaxBonus => {
+            return deburr(almanaxBonus.toLowerCase()).indexOf(keyword) !== -1;
+        });
+    }
+
+    _formattedFiveFirstMatching(testFunction) {
+        let currentDate = this.dofusMoment.clone();
+        let events = [];
+
+        while (events.length < 5) {
+            const currentAlmanax = get(almanaxData, currentDate.format('YYYY-MM-DD'), null);
+            if (currentAlmanax === null) {
+                break;
+            }
+
+            if (testFunction(currentAlmanax.bonus)) {
+                events.push({
+                    date: currentDate.format('DD/MM/YYYY'),
+                    almanaxBonus: currentAlmanax.bonus
+                });
+            }
+            currentDate.add(1, 'days');
+        }
+
+        if (events.length === 0) {
+            return "Désolé, je n'ai rien trouvé :(";
+        }
+        const formattedList = map(events, event => {
+            return `${event.date} : ${this._trimBonus(event.almanaxBonus)}`
+        }).join(';\n');
+        return `Voici ce que j'ai trouvé,\n${formattedList}`;
     }
 
     _trimBonus(bonus) {
--- app/dispatcher.js
+++ app/dispatcher.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const filter = require('lodash').filter;
+const deburr = require('lodash').deburr;
 
 const AdminController = require('./controllers/admin');
 const AlmanaxController = require('./controllers/almanax');
@@ -15,6 +16,9 @@ class Dispatcher {
             { text: '!servers', action: this.adminController.getServers.bind(this.adminController) },
             { text: '!uptime', action: this.adminController.getUptime.bind(this.adminController) },
             { rule: /almanax.*demain/i, action: this.almanaxController.getEventForTomorrow.bind(this.almanaxController) },
+            { rule: /almanax.*(butin|drop)/i, action: this.almanaxController.getNextDropEvents.bind(this.almanaxController) },
+            { rule: /almanax.*(xp|experience)/i, action: this.almanaxController.getNextXpEvents.bind(this.almanaxController) },
+            { rule: /almanax.*(economi|chasseur|paysan|pecheur|bucheron|mineur|alchi|tailleur|cordo|bijou)/i, action: this.almanaxController.getNextEventsForKeyword.bind(this.almanaxController) },
             { rule: /almanax/i, action: this.almanaxController.getEventForToday.bind(this.almanaxController) }
         ];
     }
@@ -24,6 +28,7 @@ class Dispatcher {
 
         console.log("Processing : ", message.content);
 
+        const neutralMessageContent = deburr(message.content);
         const authorIsAdmin = message.author.tag === this.config['admin_tag'];
 
         let matchedRules = filter(this.dispatchRules, dispatchRule => {
@@ -31,16 +36,21 @@ class Dispatcher {
                 if (! authorIsAdmin && dispatchRule.text[0] === '!') {
                     return false;
                 }
-                return dispatchRule.text === message.content;
+                return dispatchRule.text === neutralMessageContent;
             }
             if (dispatchRule.rule) {
-                return dispatchRule.rule.test(message.content);
+                const match = neutralMessageContent.match(dispatchRule.rule);
+                if (match) {
+                    dispatchRule.args = match.slice(1);
+                    return true;
+                }
             }
             return false;
         });
 
         if (matchedRules.length) {
-            message.reply(matchedRules[0].action());
+            const ruleToExecute = matchedRules[0]
+            message.reply(ruleToExecute.action(...(ruleToExecute.args || [])));
         } else {
             message.reply("Je n'ai pas très bien compris, désolé.");
         }
